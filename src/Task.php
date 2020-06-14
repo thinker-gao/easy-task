ba9abd5244c2331ebba9dbe9a18078a7666994de
chensir02 <18719050962@163.com>
2020-06-14 08:42:45 +0800

    support async

--- src/Task.php
+++ src/Task.php
@@ -209,11 +209,10 @@ class Task
      * @param string $alas 任务别名
      * @param int|float|string $time 定时器间隔
      * @param int $used 定时器占用进程数
-     * @param bool $async 是否异步执行
      * @return $this
      * @throws
      */
-    public function addClass($class, $func, $alas, $time = 1, $used = 1, $async = false)
+    public function addClass($class, $func, $alas, $time = 1, $used = 1)
     {
         if (!class_exists($class))
         {
@@ -239,7 +238,6 @@ class Task
                 'alas' => $alas,
                 'time' => $time,
                 'used' => $used,
-                'async' => (int)$async,
                 'class' => $class,
             ];
         }
@@ -257,10 +255,9 @@ class Task
      * @param string $alas 任务别名
      * @param int|float|string $time 定时器间隔
      * @param int $used 定时器占用进程数
-     * @param bool $async 是否异步执行
      * @return $this
      */
-    public function addCommand($command, $alas, $time = 1, $used = 1, $async = false)
+    public function addCommand($command, $alas, $time = 1, $used = 1)
     {
         if (!Helper::canExecuteCommand())
         {
@@ -273,7 +270,6 @@ class Task
             'alas' => $alas,
             'time' => $time,
             'used' => $used,
-            'async' => (int)$async,
             'command' => $command,
         ];
 
