ce62f1bb884058f0f025eb7ff68f1d3d9e8df604
zhangzheng <33833099@qq.com>
2020-06-18 18:12:56 +0800

    minimum support php5.5

--- src/Cron/FieldFactoryInterface.php
+++ src/Cron/FieldFactoryInterface.php
@@ -1,6 +1,10 @@
 <?php
 namespace EasyTask\Cron;
 
+/**
+ * Interface FieldFactoryInterface
+ * @package EasyTask\Cron
+ */
 interface FieldFactoryInterface
 {
     public function getField($position);
