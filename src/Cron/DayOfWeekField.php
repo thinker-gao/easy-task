94089ece43504e858385e2597e33a4609a5e5b97
zhangzheng <33833099@qq.com>
2020-06-18 18:12:10 +0800

    minimum support php5.5

--- src/Cron/DayOfWeekField.php
+++ src/Cron/DayOfWeekField.php
@@ -2,21 +2,11 @@
 namespace EasyTask\Cron;
 
 use DateTime;
-use DateTimeInterface;
 use InvalidArgumentException;
 
 /**
- * Day of week field.  Allows: * / , - ? L #.
- *
- * Days of the week can be represented as a number 0-7 (0|7 = Sunday)
- * or as a three letter string: SUN, MON, TUE, WED, THU, FRI, SAT.
- *
- * 'L' stands for "last". It allows you to specify constructs such as
- * "the last Friday" of a given month.
- *
- * '#' is allowed for the day-of-week field, and must be followed by a
- * number between one and five. It allows you to specify constructs such as
- * "the second Friday" of a given month.
+ * Class DayOfWeekField
+ * @package EasyTask\Cron
  */
 class DayOfWeekField extends AbstractField
 {
