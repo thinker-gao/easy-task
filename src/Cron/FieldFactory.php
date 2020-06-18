65c5eb4884234afce9b8fa30bebfdc44aedc28c1
zhangzheng <33833099@qq.com>
2020-06-18 18:09:09 +0800

    minimum support php5.5

---
+++ src/Cron/FieldFactory.php
@@ -0,0 +1,55 @@
+<?php
+namespace EasyTask\Cron;
+
+use InvalidArgumentException;
+
+/**
+ * CRON field factory implementing a flyweight factory.
+ *
+ * @see http://en.wikipedia.org/wiki/Cron
+ */
+class FieldFactory implements FieldFactoryInterface
+{
+    /**
+     * @var array Cache of instantiated fields
+     */
+    private $fields = [];
+
+    /**
+     * Get an instance of a field object for a cron expression position.
+     *
+     * @param int $position CRON expression position value to retrieve
+     *
+     * @return mixed
+     * @throws InvalidArgumentException if a position is not valid
+     */
+    public function getField($position)
+    {
+        if (!isset($this->fields[$position]))
+        {
+            $this->fields[$position] = $this->instantiateField($position);
+        }
+        return $this->fields[$position];
+    }
+
+    private function instantiateField($position)
+    {
+        switch ($position)
+        {
+            case CronExpression::MINUTE:
+                return new MinutesField();
+            case CronExpression::HOUR:
+                return new HoursField();
+            case CronExpression::DAY:
+                return new DayOfMonthField();
+            case CronExpression::MONTH:
+                return new MonthField();
+            case CronExpression::WEEKDAY:
+                return new DayOfWeekField();
+        }
+
+        throw new InvalidArgumentException(
+            ($position + 1) . ' is not a valid position'
+        );
+    }
+}
