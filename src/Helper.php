0646b2cea7d7d3bcc89c7bf40c0504f0a09126ea
zhangzheng <392223903@qq.com>
2020-07-22 12:45:10 +0800

    fixed cli_set_title

--- src/Helper.php
+++ src/Helper.php
@@ -33,10 +33,6 @@ class Helper
         {
             cli_set_process_title($title);
         }
-        elseif (extension_loaded('proctitle') && function_exists('setproctitle'))
-        {
-            setproctitle($title);
-        }
         restore_error_handler();
     }
 
